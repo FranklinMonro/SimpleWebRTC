72ae6deb9b837ddb613098ab89fd0692f421914d
FranklinMonro <fbg2468@gmail.com>
2024-09-09 11:21:48 +0200

    chat app

---
+++ server/signalling_server.js
@@ -0,0 +1,29 @@
+const WebSockerServer = require('ws');
+
+const wss = new WebSockerServer.Server({ port: 8081 }, () => {
+  console.log('Signalling server started on port 8081');
+});
+
+wss.broadcast = (ws, data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSockerServer.OPEN && client !== ws) {
+      client.send(data);
+    }
+  });
+}
+
+wss.on('connection', (ws) => {
+  console.log('Client connected, total clients connected: ', wss.clients.size);
+  ws.on('message', (message) => {
+    wss.broadcast(ws, message);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected, total clients connected: ', wss.clients.size);
+  });
+
+  ws.on('error', (err) => {
+    console.log('Error occurred: ', err);
+  });
+});
+
